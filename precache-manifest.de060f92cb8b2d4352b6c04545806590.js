a21b7b67d6d6850fad95d0eaa31aff08e54d4eab
carljstn <carljustinedg@gmail.com>
2020-03-19 16:26:23 +0800

    Updates

---
+++ precache-manifest.de060f92cb8b2d4352b6c04545806590.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff48471e702f1cb4d579c3812e3c0d7e",
+    "url": "/fb-messenger-chat/index.html"
+  },
+  {
+    "revision": "de21ff8480a4b92d0c9a",
+    "url": "/fb-messenger-chat/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc21fcdcff6cb27a3018",
+    "url": "/fb-messenger-chat/static/js/2.e4217f3c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/fb-messenger-chat/static/js/2.e4217f3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de21ff8480a4b92d0c9a",
+    "url": "/fb-messenger-chat/static/js/main.5bd62704.chunk.js"
+  },
+  {
+    "revision": "0153186ddeece18c6f0a",
+    "url": "/fb-messenger-chat/static/js/runtime-main.6d6004b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fb-messenger-chat/static/media/logo.5d5d9eef.svg"
+  }
+]);
